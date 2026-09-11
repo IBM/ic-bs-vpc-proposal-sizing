3d5218c662182999647e06dc069efed3b1c9f0ca
Jorge <jorge@MacBookPro.technicolor.net>
2026-09-11 10:03:49 +0200

    update versions to avoid vulnerabilities

--- front/src/app/app.config.server.ts
+++ front/src/app/app.config.server.ts
@@ -1,10 +1,14 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
+import { provideServerRoutesConfig, RenderMode } from '@angular/ssr';
 import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideServerRoutesConfig([
+      { path: '**', renderMode: RenderMode.Server }
+    ])
   ]
 };
 
